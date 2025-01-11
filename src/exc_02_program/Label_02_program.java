2d304e27c42d3e5ba755f3533f96901fa7ca7b4e
Sid431 <siddur31355@gmail.com>
2025-01-11 21:46:49 +0530

    variable program fort primative program

--- src/exc_02_program/Label_02_program.java
+++ src/exc_02_program/Label_02_program.java
@@ -10,12 +10,21 @@ public class Label_02_program {
         byte a= 20;
         short b = 500;
         int c= 40000;
-        long d= 1000008999;
-
+        long d= 100000L;
+        char e = 'c';
+        boolean  f = true;
+        double g= 78.996767676;
+        float h = 3.142f;
         System.out.println("byte value is :" +a);
         System.out.println("Short value is :" +b);
         System.out.println("int value is :"  +c);
         System.out.println("long value is : " +d);
+        System.out.println("char value is : " +e);
+        System.out.println("boolean value is : " +f);
+        System.out.println("double value is : " +g);
+        System.out.println("float value is : " +h);
+
+
 
     }
 }
