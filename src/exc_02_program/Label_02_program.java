033fe9f2f3f555481bc2832dcff9fdf1f9da1d21
Sid431 <siddur31355@gmail.com>
2025-01-11 19:56:39 +0530

    variable program

---
+++ src/exc_02_program/Label_02_program.java
@@ -0,0 +1,21 @@
+package exc_02_program;
+
+public class Label_02_program {
+
+    public static void main(String[] args) {
+
+        int age=10;
+        age=89;
+        System.out.println("age :"+age);
+        byte a= 20;
+        short b = 500;
+        int c= 40000;
+        long d= 1000008999;
+
+        System.out.println("byte value is :" +a);
+        System.out.println("Short value is :" +b);
+        System.out.println("int value is :"  +c);
+        System.out.println("long value is : " +d);
+
+    }
+}
